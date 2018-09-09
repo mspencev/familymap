3e248e23277bdc623608fc1bcd86d45a50dfd373
Mark Vickers <msvick@sandia.gov>
2018-09-08 21:38:02 -0600

    working on zooming, showing all family.

--- map.js
+++ map.js
@@ -1,12 +1,12 @@
-var w = 1050,
-    h = 580;
+var w = window.innerWidth,
+    h = window.innerHeight;
 var minZoom;
 var maxZoom;
 
 let geocodes = {}; // Place to lat lon mapping.
 
 
-var svg = d3.select("body")
+var svg = d3.select("#map-holder")
     .append("svg")
     .attr("width", w)
     .attr("height", h);
@@ -16,29 +16,44 @@ var svg = d3.select("body")
 
 var g = svg.append("g");
 
+// For projecting a spherical geomerty on a 2D screen 
+// var projection = d3.geoEquirectangular()
+//     .scale(150)
+//     .rotate([0, 0])
+//     .center([0, 0])
+//     .translate([w / 2, h / 2]);
+
+// var projection = d3.geoMercator();
 var projection = d3.geoEquirectangular()
-    .scale(150)
-    .rotate([0, 0])
-    .center([0, 0])
+    // .scale(150)
     .translate([w / 2, h / 2]);
 
 var geoPath = d3.geoPath()
     .projection(projection);
 
+
+
+    // svg.append("path")
+    // .datum(topojson.feature(uk, uk.objects.subunits))
+    // .attr("d", d3.geo.path().projection(d3.geo.mercator()));
+
+
+
+
 let countriesGroup;
 
-function zoomed() {
-    t = d3.event.transform;
-    if (countriesGroup) {
-        // countriesGroup.attr("transform","translate(" + [t.x, t.y] + ")scale(" + t.k + ")");
-    }
+// function zoomed() {
+//     t = d3.event.transform;
+//     if (countriesGroup) {
+//         // countriesGroup.attr("transform","translate(" + [t.x, t.y] + ")scale(" + t.k + ")");
+//     }
 
-}
+// }
 
 // Define map zoom behaviour
-var zoom = d3
-    .zoom()
-    .on("zoom", zoomed);
+// var zoom = d3
+//     .zoom()
+//     .on("zoom", zoomed);
 
 let mapEl = document.getElementById("map-holder");
 minZoom = Math.max(mapEl.offsetWidth / w, mapEl.offsetHeight / h);
@@ -47,8 +62,9 @@ maxZoom = 100 * minZoom;
 // define X and Y offset for centre of map to be shown in centre of holder
 midX = (mapEl.offsetWidth - minZoom * w) / 2;
 midY = (mapEl.offsetHeight - minZoom * h) / 2;
+
 // change zoom transform to min zoom and centre offsets
-svg.call(zoom.transform, d3.zoomIdentity.translate(midX, midY).scale(minZoom));
+// svg.call(zoom.transform, d3.zoomIdentity.translate(midX, midY).scale(minZoom));
 
 
 locations = []; // key:  location name, value:  "latLng": {
@@ -81,7 +97,9 @@ d3.json(
 
 function fetchFamilyTree() {
     // d3.json("https://mspencev.github.io/d3/map/family/family.json", function(err, json) {
-    d3.json("family.test.json", function (err, json) {
+    // d3.json("family.test.json", function (err, json) {
+    d3.json("family.12gen.json", function (err, json) {
+
         if (err) console.log("error fetching data");
         
         var personIdExp = RegExp('^.{4}-.{3}$');
@@ -203,7 +221,7 @@ function renderMap() {
 
     countriesGroup = g.attr("id", "map");
     // add a background rectangle
-    countriesGroup
+    const rect = countriesGroup
         .append("rect")
         .attr("x", 0)
         .attr("y", 0)
@@ -212,6 +230,8 @@ function renderMap() {
 
     //   countries = g.selectAll( "path" )
     countries = countriesGroup.selectAll("path")
+    // countries = rect.selectAll("path")
+
         .data(geoJson.features)
         .enter()
         .append("path")
@@ -255,7 +275,7 @@ function renderMap() {
         .on("click", function (d, i) {
             d3.selectAll(".country").classed("country-on", false);
             d3.select("#country" + d.properties.iso_a3).classed("country-on", true);
-            boxZoom(geoPath.bounds(d), geoPath.centroid(d), 20);
+            // boxZoom(geoPath.bounds(d), geoPath.centroid(d), 20);
         });
     // add the text to the label group showing country name
     countryLabels
@@ -308,7 +328,7 @@ function renderMarkers() {
             tooltip.style.top = `${(d3.event.pageY - 20)}px`;
         })
         .on("mouseout", (d) => {
-            // document.getElementById('tooltip').classList.remove('tooltip-active');
+            document.getElementById('tooltip').classList.remove('tooltip-active');
         });
 }
 
@@ -381,43 +401,43 @@ function createLine(parent, child) {
 // zoom to show a bounding box, with optional additional padding as percentage of box size
 function boxZoom(box, centroid, paddingPerc) {
 
-    let svgEl = document.getElementsByTagName("svg");
+    // let svgEl = document.getElementsByTagName("svg");
 
-    minXY = box[0];
-    maxXY = box[1];
-    // find size of map area defined
-    zoomWidth = Math.abs(minXY[0] - maxXY[0]);
-    zoomHeight = Math.abs(minXY[1] - maxXY[1]);
-    // find midpoint of map area defined
-    zoomMidX = centroid[0];
-    zoomMidY = centroid[1];
-    // increase map area to include padding
-    zoomWidth = zoomWidth * (1 + paddingPerc / 100);
-    zoomHeight = zoomHeight * (1 + paddingPerc / 100);
-    // find scale required for area to fill svg
-    maxXscale = svgEl.offsetWidth / zoomWidth;
-    maxYscale = svgEl.offsetHeight / zoomHeight;
-    zoomScale = Math.min(maxXscale, maxYscale);
-    // handle some edge cases
-    // limit to max zoom (handles tiny countries)
-    zoomScale = Math.min(zoomScale, maxZoom);
-    // limit to min zoom (handles large countries and countries that span the date line)
-    zoomScale = Math.max(zoomScale, minZoom);
-    // Find screen pixel equivalent once scaled
-    offsetX = zoomScale * zoomMidX;
-    offsetY = zoomScale * zoomMidY;
-    // Find offset to centre, making sure no gap at left or top of holder
-    dleft = Math.min(0, svgEl.offsetWidth / 2 - offsetX);
-    dtop = Math.min(0, svgEl.offsetHeight / 2 - offsetY);
-    // Make sure no gap at bottom or right of holder
-    dleft = Math.max(svgEl.offsetWidth - w * zoomScale, dleft);
-    dtop = Math.max(svgEl.offsetHeight - h * zoomScale, dtop);
-    // set zoom
-    svg
-        .transition()
-        .duration(500)
-        .call(
-            zoom.transform,
-            d3.zoomIdentity.translate(dleft, dtop).scale(zoomScale)
-        );
+    // minXY = box[0];
+    // maxXY = box[1];
+    // // find size of map area defined
+    // zoomWidth = Math.abs(minXY[0] - maxXY[0]);
+    // zoomHeight = Math.abs(minXY[1] - maxXY[1]);
+    // // find midpoint of map area defined
+    // zoomMidX = centroid[0];
+    // zoomMidY = centroid[1];
+    // // increase map area to include padding
+    // zoomWidth = zoomWidth * (1 + paddingPerc / 100);
+    // zoomHeight = zoomHeight * (1 + paddingPerc / 100);
+    // // find scale required for area to fill svg
+    // maxXscale = svgEl.offsetWidth / zoomWidth;
+    // maxYscale = svgEl.offsetHeight / zoomHeight;
+    // zoomScale = Math.min(maxXscale, maxYscale);
+    // // handle some edge cases
+    // // limit to max zoom (handles tiny countries)
+    // zoomScale = Math.min(zoomScale, maxZoom);
+    // // limit to min zoom (handles large countries and countries that span the date line)
+    // zoomScale = Math.max(zoomScale, minZoom);
+    // // Find screen pixel equivalent once scaled
+    // offsetX = zoomScale * zoomMidX;
+    // offsetY = zoomScale * zoomMidY;
+    // // Find offset to centre, making sure no gap at left or top of holder
+    // dleft = Math.min(0, svgEl.offsetWidth / 2 - offsetX);
+    // dtop = Math.min(0, svgEl.offsetHeight / 2 - offsetY);
+    // // Make sure no gap at bottom or right of holder
+    // dleft = Math.max(svgEl.offsetWidth - w * zoomScale, dleft);
+    // dtop = Math.max(svgEl.offsetHeight - h * zoomScale, dtop);
+    // // set zoom
+    // svg
+    //     .transition()
+    //     .duration(500)
+    //     .call(
+    //         zoom.transform,
+    //         d3.zoomIdentity.translate(dleft, dtop).scale(zoomScale)
+    //     );
 }
